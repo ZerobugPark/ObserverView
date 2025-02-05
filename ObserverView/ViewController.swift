d0ebfc5668ef80d2944bb4ada5bee8b122ee87ee
young <zerobugpark@gmail.com>
2025-02-05 16:12:21 +0900

    Initial Commit

---
+++ ObserverView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObserverView
+//
+//  Created by youngkyun park on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
