ee9bb6590a6d502c136d4fe3e3f1acd4eaa4041d
young <zerobugpark@gmail.com>
2025-02-05 16:54:08 +0900

    Observable 클래스 구현

--- ObserverView/Observer/Observable.swift
+++ ObserverView/Observer/Observable.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+
+class Observable<T> {
+    
+    
+    var closuer: ((T)->(Void))?
+    
+    var value: T  {
+        didSet {
+            closuer?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ closuer: @escaping (T)->(Void)) {
+        closuer(value)
+        self.closuer = closuer
+    }
+    
+    func lazyBind(_ closuer: @escaping (T)->(Void)) {
+        self.closuer = closuer
+    }
+    
+    
+    
+    
+    
+}
